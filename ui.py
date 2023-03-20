231af51fa219ce346f22f2d45317b9ea4417943d
leecks1119 <leecks1212@naver.com>
2023-03-20 16:39:08 +0900

    Revert "Add lbl1 in ui.py to print current date"
    This reverts commit 2afedbc355316d272a19b77bf5bd15e9c33f40c8.

--- ui.py
+++ ui.py
@@ -1,17 +1,13 @@
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, 
-                             QMessageBox, QPlainTextEdit, QHBoxLayout, QLabel)
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout
 from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
-from PyQt5.QtCore import QDate, Qt # 날짜와 주요 속성값 사용을 위해 추가
 
 class View(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.date = QDate.currentDate()
         self.initUI()
 
     def initUI(self):
-        self.lbl1 = QLabel(self.date.toString(Qt.DefaultLocaleLongDate),self)
         self.btn1=QPushButton('Message',self) # 버튼추가
         self.btn2=QPushButton('Clear',self) # 버튼2 추가
 
@@ -26,7 +22,6 @@ class View(QWidget):
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.te1)
         vbox.addLayout(hbox)
-        vbox.addWidget(self.lbl1)
         
         vbox.addStretch(1)
 
