5a9f2aee1ef1a5cb51dd5b86e992446694575c28
leecks1119 <leecks1212@naver.com>
2023-03-23 22:28:57 +0900

    Modify ui.py to add le1, le2 and cb widgets

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox
 from PyQt5.QtGui import QIcon # icon을 추가하기 위한 라이브러리
+from PyQt5 import QtCore
 
 class View(QWidget):
 
@@ -14,6 +15,20 @@ class View(QWidget):
         self.te1=QPlainTextEdit()
         self.te1.setReadOnly(True)
 
+        self.le1=QLineEdit('0',self)
+        self.le1.setAlignment(QtCore.Qt.AlignRight)
+
+        self.le2=QLineEdit('0',self)
+        self.le2.setAlignment(QtCore.Qt.AlignRight)
+
+        self.cb=QComboBox(self)
+        self.cb.addItems(['+', '-', '*', '/'])
+
+        hbox_formular = QHBoxLayout()
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         hbox=QHBoxLayout()
         hbox.addStretch(1)
         hbox.addWidget(self.btn1)
@@ -21,8 +36,8 @@ class View(QWidget):
 
         vbox=QVBoxLayout() # 수직 레이아웃 위젯 생성
         vbox.addWidget(self.te1)
+        vbox.addLayout(hbox_formular)
         vbox.addLayout(hbox)
-        
         vbox.addStretch(1)
 
         self.setLayout(vbox)
