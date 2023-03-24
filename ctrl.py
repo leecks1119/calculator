f644c4a0d6c8d5542431d8417b753b878c8052dd
leecks1119 <leecks1212@naver.com>
2023-03-24 16:08:32 +0900

    Modify pow function using exception

--- ctrl.py
+++ ctrl.py
@@ -41,7 +41,11 @@ class Control:
         return a/b
     
     def pow(sefl,a,b):
-        if (a==0):
-            return 0
-        else:
-            return pow(a,b)
+        try:
+            if (a==0):
+                raise Exception("Base Error")
+        
+        except Exception as e:
+            return e
+        
+        return pow(a,b)
